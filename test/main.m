11e51e1a180c1f6fa43417ab60a09a6589536acc
Fengming Sun <s@sfmblog.cn>
2018-12-16 21:50:52 +0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by NikSun on 2018/12/16.
+//  Copyright © 2018 NikSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
